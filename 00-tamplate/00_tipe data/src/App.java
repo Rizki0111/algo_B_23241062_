f8ff8f91be9ea1074624f51bc041b2a2ba9fcf0b
Rizki0111 <146050947+Rizki0111@users.noreply.github.com>
2023-11-28 13:00:54 +0800

    00_tipe data

---
+++ 00-tamplate/00_tipe data/src/App.java
@@ -0,0 +1,73 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    EXception {
+
+        // tipe data di java
+        // integer, byte, short, long, double, float, char, boolean
+ 
+        // integer (satuan)
+        int i = 10;
+        System.out.println("====INTEGER====");
+        System.out.println("nilai integer i = " + i);
+        System.out.println("nilai MAX : + Integer.MAX_VALUE");
+        System.out.println("nilai MIN : + Integer.MIN_VALUE");
+        System.out.println("Besar Integer : " + Integer.SIZE + "bit");
+}
+        // byte
+        byte b = 10;
+        System.out.println("====BYTE====");
+        System.out.println("nilai byte b = " + b);
+        System.out.println("nilai MAX : + Byte.MAX_VALUE");
+        System.out.println("nilai MIN : + Byte.MIN_VALUE");
+        System.out.println("Besar BYte : " + Byte.SIZE + "bit");
+
+        // short
+        short s = 10;
+        System.out.println("====SHORT====");
+        System.out.println("nilai short s = " + s);
+        System.out.println("nilai MAX : + Short.MAX_VALUE");
+        System.out.println("nilai MIN : + Short.MIN_VALUE");
+        System.out.println("Besar Short : " + Short.SIZE + "bit");
+
+        // long
+        long l= 10;
+        System.out.println("====LONG====");
+        System.out.println("nilai long l = " + l);
+        System.out.println("nilai MAX : + Long.MAX_VALUE");
+        System.out.println("nilai MIN : + Long.MIN_VALUE");
+        System.out.println("Besar Long : " + Long.SIZE + "bit");
+
+        // double
+        double d= 10;
+        System.out.println("====DOUBLE====");
+        System.out.println("nilai double d = " + d);
+        System.out.println("nilai MAX : + Double.MAX_VALUE");
+        System.out.println("nilai MIN : + Double.MIN_VALUE");
+        System.out.println("Besar Double : " + Double.SIZE + "bit");
+
+        // float
+        float f = 10;
+        System.out.println("====FLOAT====");
+        System.out.println("nilai float f = " + f);
+        System.out.println("nilai MAX : + Float.MAX_VALUE");
+        System.out.println("nilai MIN : + Float.MIN_VALUE");
+        System.out.println("Besar Float : " + Float.SIZE + "bit");
+
+        // char
+        char c = 10;
+        System.out.println("====CHAR====");
+        System.out.println("nilai char c = " + c);
+        System.out.println("nilai MAX : + Char.MAX_VALUE");
+        System.out.println("nilai MIN : + Char.MIN_VALUE");
+        System.out.println("Besar Char : " + Char.SIZE + "bit");
+
+        // boolean
+        boolean b = 10;
+        System.out.println("====BOOLEAN====");
+        System.out.println("nilai boolean b = " + b);
+        System.out.println("nilai MAX : + Boolean.MAX_VALUE");
+        System.out.println("nilai MIN : + Boolean.MIN_VALUE");
+        System.out.println("Besar boolean : " + Boolean.SIZE + "bit");
+
+        
+
